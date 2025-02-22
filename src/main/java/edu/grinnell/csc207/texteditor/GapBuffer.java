a8743a95d3bbbaa528a9546db0ecf651e25f33e1
Aubrey Woodrow <woodrowa@grinnell.edu>
2025-02-22 23:56:16 +0000

    Copied and expanded tests for gap buffer testing, added javadoc comments for gap buffer methods

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -14,12 +14,20 @@ public class GapBuffer {
     
     private static final int INITIAL_SIZE = 10;
     
+    /**
+     * Creates a new empty gap buffer with the cursor at the start and the
+     * after-cursor text starting position out of bounds to show nothing is present.
+     */
     public GapBuffer() {
         buffer = new char[INITIAL_SIZE];
         gapStartIndex = 0;
         textStartIndex = INITIAL_SIZE;
     }
     
+    /**
+     * Inserts the character at the cursor position, expanding the gap buffer if it's full
+     * @param ch the character to be inserted
+     */
     public void insert(char ch) {
         if (gapStartIndex==textStartIndex) {
             expandBuffer();
@@ -27,60 +35,101 @@ public class GapBuffer {
         buffer[gapStartIndex++] = ch;
     }
     
+    /**
+     * Creates a new gap buffer of double the size, keeping the existing gap structure
+     * and cursor placement (helper function for insert).
+     */
     private void expandBuffer() {
         char[] bigger = new char[buffer.length * 2];
         int index;
-        for (index = 0; index <= gapStartIndex; index++) {
-            bigger[index] = buffer[index];
+        if (gapStartIndex == buffer.length) {
+            for (index = 0; index < buffer.length; index++) {
+                bigger[index] = buffer[index];
+                textStartIndex = bigger.length;
+            }
+        } else {
+            for (index = 0; index < gapStartIndex; index++) {
+                bigger[index] = buffer[index];
+            }
+            for (index = 1; index <= (buffer.length - textStartIndex); index++) {
+                bigger[bigger.length - index] = buffer[buffer.length - index];
+            }
+            textStartIndex = bigger.length - index + 1;
         }
-        gapStartIndex++;
-        for (index = 1; index <= (buffer.length - textStartIndex); index++) {
-            bigger[bigger.length - index] = buffer[buffer.length - index];
-        }
-        textStartIndex = bigger.length - index + 1;
         buffer = bigger;
     }
 
+    /**
+     * Removes a character from the buffer by considering it part of the gap.
+     */
     public void delete() {
         if (gapStartIndex != 0) {
             gapStartIndex--;
         }
     }
 
+    /**
+     * Returns the current position of the cursor in the buffer.
+     * @return location of the start of the gap, aka the cursor position
+     */
     public int getCursorPosition() {
         return gapStartIndex;
     }
 
+    /**
+     * Moves the cursor one left by sending the character immediately left of the cursor 
+     * to the other side of the gap.
+     */
     public void moveLeft() {
         if (gapStartIndex != 0) {
             buffer[--textStartIndex] = buffer[--gapStartIndex];
         }
     }
 
+    /** 
+     * Moves the cursor one right by sending the character immediately right of the
+     * cursor to the other side of the gap.
+     */
     public void moveRight() {
         if (textStartIndex != buffer.length) {
             buffer[gapStartIndex++] = buffer[textStartIndex++];
         }
     }
 
+    /**
+     * Gets the size of the buffer
+     * @return the number of characters "used" in the buffer (size)
+     */
     public int getSize() {
         return buffer.length - (textStartIndex - gapStartIndex);
     }
 
+    /**
+     * Gets the character at the given index of the buffer, ignoring the gap indices.
+     * @param i index of the desired character
+     * @return character at the index
+     */
     public char getChar(int i) {
-        if (i < gapStartIndex) {
+        if (i < 0 || i >= buffer.length) {
+            throw new IndexOutOfBoundsException("Not a valid index.");
+        } else if (i < gapStartIndex) {
             return buffer[i];
         } else {
             return buffer[i + (textStartIndex - gapStartIndex)];
         }
     }
 
+    /**
+     * Provides a string representation of the object
+     * @return String that buffer contains, ignoring the gap chars
+     */
     @Override
     public String toString() {
         String ret = "";
         for (int i = 0; i < buffer.length; i++) {
-            if (i < gapStartIndex || i >= textStartIndex);
-            ret += buffer[i];
+            if (i < gapStartIndex || i >= textStartIndex) {
+                ret += buffer[i];
+            }
         }
         return ret;
     }
--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -1,5 +1,150 @@
 package edu.grinnell.csc207.texteditor;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+
+
 public class GapBufferTests {
-    /** TODO: fill me in with unit and property tests! */
+    
+    @Test
+    public void outOfBoundsTests() {
+        GapBuffer b = new GapBuffer();
+        assertThrows(IndexOutOfBoundsException.class, ()->b.getChar(11));
+        assertThrows(IndexOutOfBoundsException.class, ()->b.getChar(-4));
+    }
+    
+    @Test
+    public void movementTests() {
+        GapBuffer b = new GapBuffer();
+        b.insert('h');
+        b.insert('i');
+        b.insert('!');
+        b.moveRight();
+        assertEquals(3, b.getCursorPosition());
+        b.moveLeft();
+        assertEquals(2, b.getCursorPosition());
+        b.moveLeft();
+        assertEquals(1, b.getCursorPosition());
+        b.moveRight();
+        assertEquals(2, b.getCursorPosition());
+        b.moveLeft();
+        b.moveLeft();
+        assertEquals(0, b.getCursorPosition());
+        b.moveLeft();
+        assertEquals(0, b.getCursorPosition());
+    }
+    
+    @Test
+    public void frontOfBufferTests() {
+        GapBuffer b = new GapBuffer();
+        b.insert('c');
+        b.insert('o');
+        b.insert('m');
+        b.insert('p');
+        b.insert('u');
+        b.insert('t');
+        b.insert('e');
+        b.insert('r');
+        for (int i = 0; i < b.getSize(); i++) {
+            b.moveLeft();
+        }
+        b.delete();
+        assertEquals("computer", b.toString());
+        assertEquals(0, b.getCursorPosition());
+        b.insert('a');
+        b.insert(' ');
+        assertEquals("a computer", b.toString());
+        assertEquals(2, b.getCursorPosition());
+    }
+    
+    @Test
+    public void endOfBufferTests() {
+        GapBuffer b = new GapBuffer();
+        b.insert('h');
+        b.insert('e');
+        b.insert('l');
+        b.insert('l');
+        b.insert('o');
+        assertEquals(5, b.getCursorPosition());
+        b.moveRight();
+        assertEquals(5, b.getCursorPosition());
+        b.insert('!');
+        b.insert('!');
+        assertEquals(7, b.getCursorPosition());
+        assertEquals("hello!!", b.toString());
+    }
+    
+    @Test
+    public void middleOfBufferTests() {
+        GapBuffer b = new GapBuffer();
+        b.insert('r');
+        b.insert('a');
+        b.insert('c');
+        b.insert('e');
+        b.insert('c');
+        b.insert('a');
+        b.insert('r');
+        assertEquals(7, b.getCursorPosition());
+        b.moveLeft();
+        b.moveLeft();
+        b.moveLeft();
+        assertEquals(4, b.getCursorPosition());
+        b.insert('r');
+        b.insert('a');
+        b.insert('c');
+        b.insert('e');
+        b.moveLeft();
+        b.moveLeft();
+        b.moveLeft();
+        b.moveLeft();
+        b.insert('c');
+        b.insert('a');
+        b.insert('r');
+        assertEquals(7, b.getCursorPosition());
+        assertEquals("racecarracecar", b.toString());
+    }
+    
+    @Test
+    public void expansionTests() {
+        GapBuffer b = new GapBuffer();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < 1000; i++) {
+            b.insert(alphabet.charAt(i % 26));
+        }
+        assertEquals('l',b.getChar(999));
+    }
+    
+    @Property
+    public boolean defaultSizeTests(@ForAll @IntRange(min = 0, max = 100) int size) {
+        GapBuffer b = new GapBuffer();
+        for (int i = 0; i < size; i++) {
+            b.insert('a');
+        }
+        return b.getSize() == size;
+    }
+    
+    @Property
+    public boolean deleteSizeTests(@ForAll @IntRange(min = 1, max = 100) int size) {
+        GapBuffer b = new GapBuffer();
+        for (int i = 0; i < size; i++) {
+            b.insert('a');
+        }
+        b.delete();
+        return b.getSize() == size - 1;
+    }
+    
+    @Property
+    public boolean insertSizeTests(@ForAll @IntRange(min = 0, max = 100) int size) {
+        GapBuffer b = new GapBuffer();
+        for (int i = 0; i < size; i++) {
+            b.insert('a');
+        }
+        b.insert('b');
+        return b.getSize() == size + 1;
+    }
 }
