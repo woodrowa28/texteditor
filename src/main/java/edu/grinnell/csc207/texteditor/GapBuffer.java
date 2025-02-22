d0424fd94f09e50551076eb8f218f537cf87d040
Aubrey Woodrow <woodrowa@grinnell.edu>
2025-02-22 21:48:42 +0000

    Added insert and delete for the gap buffer

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -5,12 +5,46 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
+    
+    private int[] buffer;
+    
+    private int gapStartIndex;
+    
+    private int textStartIndex;
+    
+    private static final int INITIAL_SIZE = 10;
+    
+    public GapBuffer() {
+        buffer = new int[INITIAL_SIZE];
+        gapStartIndex = 0;
+        textStartIndex = INITIAL_SIZE;
+    }
+    
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if(gapStartIndex==textStartIndex) {
+            expandBuffer();
+        }
+        buffer[gapStartIndex++] = ch;
+    }
+    
+    private void expandBuffer() {
+        int[] bigger = new int[buffer.length * 2];
+        int index;
+        for (index = 0; index <= gapStartIndex; index++) {
+            bigger[index] = buffer[index];
+        }
+        gapStartIndex++;
+        for (index = 1; index <= (buffer.length - textStartIndex); index++) {
+            bigger[bigger.length - index] = buffer[buffer.length - index];
+        }
+        textStartIndex = bigger.length - index + 1;
+        buffer = bigger;
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (gapStartIndex != 0) {
+            gapStartIndex--;
+        }
     }
 
     public int getCursorPosition() {
