2650bb245183847ea2e71d30c858027f9f1eaa4c
Aubrey Woodrow <woodrowa@grinnell.edu>
2025-02-23 01:05:42 +0000

    Basic program loop in main

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -2,6 +2,8 @@
 package edu.grinnell.csc207.texteditor;
 
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
@@ -26,8 +28,37 @@ public class TextEditor {
         String path = args[0];
         System.out.format("Loading %s...\n", path);
         
+        GapBuffer buffer = new GapBuffer();
         Screen screen = new DefaultTerminalFactory().createScreen();
         screen.startScreen();
+        boolean stillRunning = true;
+        while(stillRunning) {
+            KeyStroke stroke = screen.readInput();
+            KeyType type = stroke.getKeyType();
+            switch (type) {
+                case Character:
+                    char ch = stroke.getCharacter();
+                    buffer.insert(ch);
+                    break;
+                case ArrowLeft:
+                    buffer.moveLeft();
+                    break;
+                case ArrowRight:
+                    buffer.moveRight();
+                    break;
+                case Backspace:
+                    buffer.delete();
+                    break;
+                case Escape:
+                    stillRunning = false;
+                    break;
+            }
+            drawBuffer(buffer, screen);
+        }
         screen.stopScreen();
+    }
+    
+    public static void drawBuffer(GapBuffer buf, Screen screen) {
+        
     }
 }
